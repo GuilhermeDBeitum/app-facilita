c3157ac95736be277442b3d182fb765e279f1bc9
xpointersite@outlook.com <xpointersite@outlook.com>
2021-07-20 21:37:10 -0300

    Firebase + Login (Router + Vuex)

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
 
-Vue.use(Vuex)
+const SET_LOGIN = "SET_LOGIN";
+const SET_PASSWORD = "SET_PASSWORD";
 
-export default new Vuex.Store({
+const moduleInfo = {
   state: {
+    login: null,
+    password: null,
+  },
+  getters: {
+    login: (state) => {
+      return state.login;
+    },
+    password: (state) => {
+      return state.password;
+    },
   },
   mutations: {
+    [SET_LOGIN](state, login) {
+      state.login = login;
+    },
+    [SET_PASSWORD](state, password) {
+      state.password = password;
+    },
   },
-  actions: {
-  },
+}
+  
+const store = new Vuex.Store({
   modules: {
-  }
-})
+    modinfo: moduleInfo,
+  },
+});
+
+export default store;
---
+++ src/views/Painel.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app id="inspire">
+    <Head />
+  </v-app>
+</template>
+
+<script>
+import Head from "../components/Header.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Head,
+  },
+
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
